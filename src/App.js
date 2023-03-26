8dac468b10e86b5bfeb422fac4130185485f0c1c
valkwek <valeriekwek3@gmail.com>
2023-03-26 18:27:25 -0400

    Part 3

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Rick and Morty Characters
-        </p>
-        <a
-          className="App-link"
-          href="https://rickandmortyapi.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Rick and Morty API
-        </a>
-      </header>
-    </div>
-  );
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Rick and Morty Characters
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://rickandmortyapi.com/"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Rick and Morty API
+//        </a>
+//      </header>
+//    </div>
+//  );
 }
 
 const url = 'https://rickandmortyapi.com/api/location/';
@@ -49,6 +49,7 @@ fetch(url)
             const characterName = characterData.name;
             const characterStatus = characterData.status;
             const characterImageUrl = characterData.image;
+            let notes = localStorage.getItem(characterName) || '';
 
             const characterContainerElement = document.createElement('li');
             characterContainerElement.classList.add('character-container');
@@ -70,6 +71,7 @@ fetch(url)
                   const characterStatus = characterData.status;
                   const characterLocation = characterData.location.name;
                   const characterImageUrl = characterData.image;
+                  let notes = localStorage.getItem(characterName) || '';
 
                   const characterDetailsElement = document.createElement('div');
                   characterDetailsElement.classList.add('character-details');
@@ -78,11 +80,46 @@ fetch(url)
                     <img src="${characterImageUrl}" alt="${characterName}">
                     <p>Status: ${characterStatus}</p>
                     <p>Location: ${characterLocation}</p>
+                    <p>Notes: ${notes}</p>
+                    <textarea class="notes" placeholder="Add your notes here"></textarea>
+                    <button id="back-btn">Back to Characters</button>
                   `;
 
                   const bodyElement = document.querySelector('body');
                   bodyElement.innerHTML = '';
                   bodyElement.appendChild(characterDetailsElement);
+
+                  const notesElement = characterDetailsElement.querySelector('.notes');
+                  notesElement.addEventListener('change', () => {
+                    // save the notes to local storage
+                    const notes = notesElement.value;
+                    localStorage.setItem(characterName, notes);
+
+                    // send the notes to a dummy API endpoint
+                    const endpoint = 'https://jsonplaceholder.typicode.com/posts';
+                    fetch(endpoint, {
+                      method: 'POST',
+                      headers: {
+                        'Content-Type': 'application/json'
+                      },
+                      body: JSON.stringify({
+                        characterName: characterName,
+                        notes: notes
+                      })
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                      console.log(data);
+                    })
+                    .catch(error => console.error(error));
+                  });
+
+                  // add event listener to back button
+                  const backBtn = document.getElementById('back-btn');
+                  backBtn.addEventListener('click', () => {
+                    bodyElement.innerHTML = '';
+                    bodyElement.appendChild(locationElement);
+                  });
                 })
                 .catch(error => console.error(error));
             });
